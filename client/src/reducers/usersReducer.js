6b51df24f876e4ca8a73abb188717a11bea8d9db
AndresHiguitaAMG <andreshiguitaamg@gmail.com>
2023-08-22 22:26:28 -0500

    implementing useReducer

---
+++ client/src/reducers/usersReducer.js
@@ -0,0 +1,15 @@
+export const usersReducer = (state = [], action) => {
+    switch (action.type) {
+        // eslint-disable-next-line no-undef
+        case addUser:
+            return [
+                ...state,
+                {
+                    ...action.payload,
+                    id: new Date().getTime(),
+                }
+            ];
+        default:
+            return state;
+    }
+}
