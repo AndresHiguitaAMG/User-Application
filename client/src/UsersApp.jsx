c14e3281ae59a58ae02f0d2cef1fbf591c07a7cb
AndresHiguitaAMG <andreshiguitaamg@gmail.com>
2023-09-04 17:18:13 -0500

    Add CSS styles and create a new modal form component

--- client/src/UsersApp.jsx
+++ client/src/UsersApp.jsx
@@ -1,4 +1,5 @@
 import { UserForm } from "./components/UserForm"
+import { UserModalForm } from "./components/UserModalForm";
 import { UsersList } from "./components/UsersList"
 import { useUsers } from "./hooks/useUsers"
 
@@ -18,28 +19,13 @@ export const UsersApp = () => {
   return (
     <>
       {
-        !visibleForm || 
-          <div className="abrir-modal animacion fadeIn">
-            <div className="modal" style={{display: "block"}} tabIndex="-1">
-              <div className="modal-dialog" role="document">
-                <div className="modal-content">
-                  <div className="modal-header">
-                    <h5 className="modal-title">
-                      {userSelected.id > 0 ? "Editar" : "Crear"} Modal Usuarios
-                    </h5>
-                  </div>
-                  <div className="modal-body">
-                    <UserForm
-                      handlerAddUser={handlerAddUser}
-                      initalUserForm={initalUserForm}
-                      userSelected={userSelected}
-                      handlerCloseForm={handlerCloseForm}
-                    />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+        !visibleForm ||
+          <UserModalForm
+            userSelected={userSelected}
+            initalUserForm={initalUserForm}
+            handlerAddUser={handlerAddUser}
+            handlerCloseForm={handlerCloseForm}
+          />
       }
       <div className="container my-4">
         <h2>Users App</h2>
@@ -66,8 +52,8 @@ export const UsersApp = () => {
             handlerUserSelectedForm={handlerUserSelectedForm}
           />
           }
-          </div>
         </div>
+      </div>
       </div>
     </>
   )
