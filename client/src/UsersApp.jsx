391fe5ae74c31e2c8638462702947361641c1d92
AndresHiguitaAMG <andreshiguitaamg@gmail.com>
2023-09-04 17:02:20 -0500

    modal form window

--- client/src/UsersApp.jsx
+++ client/src/UsersApp.jsx
@@ -16,31 +16,45 @@ export const UsersApp = () => {
   } = useUsers();
 
   return (
-  <div className="container my-4">
-    <h2>Users App</h2>
-    <div className="row">
-        {
-        !visibleForm ||
+    <>
+      {
+        !visibleForm || 
+          <div className="abrir-modal animacion fadeIn">
+            <div className="modal" style={{display: "block"}} tabIndex="-1">
+              <div className="modal-dialog" role="document">
+                <div className="modal-content">
+                  <div className="modal-header">
+                    <h5 className="modal-title">
+                      {userSelected.id > 0 ? "Editar" : "Crear"} Modal Usuarios
+                    </h5>
+                  </div>
+                  <div className="modal-body">
+                    <UserForm
+                      handlerAddUser={handlerAddUser}
+                      initalUserForm={initalUserForm}
+                      userSelected={userSelected}
+                      handlerCloseForm={handlerCloseForm}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+      }
+      <div className="container my-4">
+        <h2>Users App</h2>
+      <div className="row">
         <div className="col">
-          <UserForm
-            handlerAddUser={handlerAddUser}
-            initalUserForm={initalUserForm}
-            userSelected={userSelected}
-            handlerCloseForm={handlerCloseForm}
-          />
-        </div>
-        }
-      <div className="col">
-        {
-        visibleForm ||
-        <button 
+          {
+          visibleForm ||
+          <button
           className="btn btn-primary my-2"
           onClick={handlerOpenForm}
-        >
-          New user
-        </button>
-        }
-        {
+          >
+            New user
+          </button>
+          }
+          {
           users.length === 0 ?
           <div className="alert alert-warning">
             No hay usuarios en el sistema
@@ -51,9 +65,10 @@ export const UsersApp = () => {
             users={users}
             handlerUserSelectedForm={handlerUserSelectedForm}
           />
-        }
+          }
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
+    </>
   )
 }
