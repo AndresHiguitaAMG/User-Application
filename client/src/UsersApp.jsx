6d906e9a6da31eb1915c86c5128946c020cbb78a
AndresHiguitaAMG <andreshiguitaamg@gmail.com>
2023-09-23 11:36:31 -0500

    Hook Auth

--- client/src/UsersApp.jsx
+++ client/src/UsersApp.jsx
@@ -1,39 +1,15 @@
-import { useReducer } from "react"
-import { LoginPage } from "./auth/pages/LoginPage"
-import { loginReducer } from "./auth/reducers/loginReducer"
-import { UsersPage } from "./pages/UsersPage"
-import Swal from "sweetalert2"
+import { LoginPage } from "./auth/pages/LoginPage";
+import { UsersPage } from "./pages/UsersPage";
 import { NavBar } from "./components/layout/NavBar"
+import { useAuth } from "./auth/hooks/useAuth";
 
-const initialLogin = JSON.parse(sessionStorage.getItem("login")) || {
-  isAuth: false,
-  user: undefined,
-}
 export const UsersApp = () => {
-  const [login, dispatch] = useReducer(loginReducer, initialLogin);
 
-  const handlerLogin = ({username, password}) => {
-    if (username === "admin" && password === "12345") {
-      const user = {username: "admin"}
-      dispatch({
-        type: "login",
-        payload: user,
-      });
-      sessionStorage.setItem("login", JSON.stringify({
-        isAuth: true,
-        user,
-      }));
-    } else {
-      Swal.fire("Error login", "username o password invalidos", "error");
-    }
-  };
-
-  const handlerLogout = () => {
-    dispatch({
-      type: "logout",
-    });
-    sessionStorage.removeItem("login");
-  }
+  const {
+    login,
+    handlerLogin,
+    handlerLogout
+  } = useAuth();
 
   return (
     <>
@@ -56,4 +32,4 @@ export const UsersApp = () => {
       }
     </>
   )
-}
+};
